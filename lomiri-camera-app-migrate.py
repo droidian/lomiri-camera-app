057cc4064f4546098a30df5648b8625ef579ae6f
Guido Berhoerster <guido+ubports@berhoerster.name>
2023-03-23 16:11:08 +0100

    Handle migration edge case
    Handle the case where both the old and new Pictures or Videos directory exist.

--- lomiri-camera-app-migrate.py
+++ lomiri-camera-app-migrate.py
@@ -15,13 +15,15 @@ new_config_file = xdg_config_home / organization / f"{application}.conf"
 if old_config_file.is_file() and not new_config_file.exists():
     old_config_file.rename(new_config_file)
 
-pictures_path = Path.home() / "Pictures" / old_application
-if pictures_path.is_dir():
-    pictures_path.rename(pictures_path.parent / application)
+old_pictures_path = Path.home() / "Pictures" / old_application
+pictures_path = Path.home() / "Pictures" / application
+if old_pictures_path.is_dir() and not pictures_path.exists():
+    old_pictures_path.rename(pictures_path)
 
-videos_path = Path.home() / "Videos" / old_application
-if videos_path.is_dir():
-    videos_path.rename(videos_path.parent / application)
+old_videos_path = Path.home() / "Videos" / old_application
+videos_path = Path.home() / "Videos" / application
+if old_videos_path.is_dir() and not videos_path.exists():
+    old_videos_path.rename(videos_path)
 
 if len(sys.argv) > 1:
     os.execvp(sys.argv[1], sys.argv[1:])
