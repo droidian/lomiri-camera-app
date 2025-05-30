droidian/lomiri-camera-app

commit e38dbba166474518f53b61919f2ced9520189e90

add fixes to barcodereader app and drop obsolete imports

qml/Viewfinder/ViewFinderOverlay.qml
@@ -951,6 +951,20 @@ Item {
             }
         }
 
+        FileOperations {
+            id: fileOperations
+        }
+
+        function decimal2sexagesimal(coord) {
+            var degree = Math.trunc(coord)
+            var decimals = coord - degree
+            var minutes = Math.trunc(decimals * 60)
+            var minDecimals = (decimals * 60) - minutes
+            var seconds = minDecimals * 60  //keep decimals here for higher precision
+            var sexaCoord = degree.toString() + "/1" + " " + minutes.toString() + "/1" + " " + seconds.toFixed(0).toString() + "/1" //zeros equal to number of decimals
+            return sexaCoord
+        }
+
         function switchCamera() {
             camera.switchInProgress = true;
             //                viewFinderGrab.sourceItem = viewFinder;
@@ -1034,6 +1048,10 @@ Item {
                                                        viewFinderOverlay.settings.dateStampColor,
                                                        viewFinderOverlay.settings.dateStampOpacity,
                                                        viewFinderOverlay.settings.dateStampAlign);
+                }
+                if (settings.hasEXIF && settings.gpsEnabled && positionSource.isPrecise) {
+                    var position = positionSource.position;
+                    fileOperations.setEXIFData(path, controls.decimal2sexagesimal(position.coordinate.latitude), controls.decimal2sexagesimal(position.coordinate.longitude));
                 }
             }
         }

qml/Viewfinder/BarcodeReaderOverlay.qml
@@ -26,6 +26,8 @@ import CameraApp 0.1
 import Qt.labs.settings 1.0
 import QtGraphicalEffects 1.0
 
+import "../components"
+
 Item {
     id: viewFinderOverlay
 

qml/barcode-reader-app.qml
@@ -24,6 +24,9 @@ import CameraApp 0.1
 import Qt.labs.settings 1.0
 import QZXing 3.3
 
+import "components"
+import "Viewfinder"
+
 Window {
     id: main
     objectName: "main"

qml/components/PhotogridView.qml
@@ -22,7 +22,6 @@ import Lomiri.Thumbnailer 0.1
 import Lomiri.Content 1.3
 import Qt.labs.settings 1.0
 import CameraApp 0.1
-import "qml/components"
 import "MimeTypeMapper.js" as MimeTypeMapper
 
 FocusScope {

qml/components/VideoReview.qml
@@ -17,7 +17,6 @@
 
 import QtQuick 2.12
 import Lomiri.Components 1.3
-import "qml/components"
 
 Item {
     property string videoPath

commit f3f7476729771137ea510f81357fef98d9d3b9bb

qml/Viewfinder: fix content export preview corruption in !hasEXIF mode
Because of signal connection order, content export preview will start
rendering image while `deleteEXIF()` re-writes the file. This causes
preview image to corrupt.

Move all imageSaved() signal handler code from `ViewFinderOverlay` to
`ViewFinderView` so that we can control the ordering. Order it so that
(synchronous) `deleteEXIF()` happens before content export preview
renders image.

While we're at it, remove call to non-existent `fileOperations.
setEXIFData()` (which is redundant with another part of code in
`ViewFinderOverlay`); the exception thrown will prevent the rest of the
code to run.

Finally, add a few comments so that we turn `deleteEXIF()` to be
asynchronous in the future.

Fixes: https://gitlab.com/ubports/development/apps/lomiri-camera-app/-/issues/217
Fixes: e38dbba16647 ("add fixes to barcodereader app and drop obsolete imports")

qml/Viewfinder/ViewFinderOverlay.qml
@@ -951,20 +951,6 @@ Item {
             }
         }
 
-        FileOperations {
-            id: fileOperations
-        }
-
-        function decimal2sexagesimal(coord) {
-            var degree = Math.trunc(coord)
-            var decimals = coord - degree
-            var minutes = Math.trunc(decimals * 60)
-            var minDecimals = (decimals * 60) - minutes
-            var seconds = minDecimals * 60  //keep decimals here for higher precision
-            var sexaCoord = degree.toString() + "/1" + " " + minutes.toString() + "/1" + " " + seconds.toFixed(0).toString() + "/1" //zeros equal to number of decimals
-            return sexaCoord
-        }
-
         function switchCamera() {
             camera.switchInProgress = true;
             //                viewFinderGrab.sourceItem = viewFinder;
@@ -1024,10 +1010,6 @@ Item {
                                           position.horizontalAccuracy <= 100)
         }
 
-        PostProcessOperations {
-            id: postProcessOperations
-        }
-
         Connections {
             target: camera.imageCapture
             onReadyChanged: {
@@ -1035,23 +1017,6 @@ Item {
                     if (camera.switchInProgress) {
                         controls.completeSwitch();
                     }
-                }
-            }
-            onImageSaved : {
-                if(path &&!settings.hasEXIF)
-                {
-                    postProcessOperations.deleteEXIFdata(path);
-                }
-                if(path && settings.dateStampImages && !main.contentExportMode) {
-                    postProcessOperations.addDateStamp(path,
-                                                       viewFinderOverlay.settings.dateStampFormat,
-                                                       viewFinderOverlay.settings.dateStampColor,
-                                                       viewFinderOverlay.settings.dateStampOpacity,
-                                                       viewFinderOverlay.settings.dateStampAlign);
-                }
-                if (settings.hasEXIF && settings.gpsEnabled && positionSource.isPrecise) {
-                    var position = positionSource.position;
-                    fileOperations.setEXIFData(path, controls.decimal2sexagesimal(position.coordinate.latitude), controls.decimal2sexagesimal(position.coordinate.longitude));
                 }
             }
         }

qml/Viewfinder/ViewFinderView.qml
@@ -131,7 +131,24 @@ FocusScope {
             }
 
             onImageSaved: {
-                if (main.contentExportMode) viewFinderExportConfirmation.mediaPath = path;
+                if(!viewFinderOverlay.settings.hasEXIF) {
+                    // TODO: make this operation asynchronous.
+                    postProcessOperations.deleteEXIFdata(path);
+                }
+
+                if(viewFinderOverlay.settings.dateStampImages && !main.contentExportMode) {
+                    postProcessOperations.addDateStamp(path,
+                                                       viewFinderOverlay.settings.dateStampFormat,
+                                                       viewFinderOverlay.settings.dateStampColor,
+                                                       viewFinderOverlay.settings.dateStampOpacity,
+                                                       viewFinderOverlay.settings.dateStampAlign);
+                }
+
+                // TODO: make it so that asynchronous operations signals to us
+                // on finished and only then proceed to the code below.
+
+                if (main.contentExportMode)
+                    viewFinderExportConfirmation.mediaPath = path;
 
                 viewFinderView.photoTaken(path);
                 metricPhotos.increment();
@@ -367,6 +384,10 @@ FocusScope {
         asynchronous: true
         anchors.fill: parent
         sourceComponent: viewFinderExportConfirmationComp
+    }
+
+    PostProcessOperations {
+        id: postProcessOperations
     }
 
     property alias viewFinderExportConfirmation: viewFinderExportConfirmationLoader.item